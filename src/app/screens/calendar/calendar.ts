a75d35ab43cc5d054147386591636b0af0826cd6
Yaroslav Khyzhniak <yaroslav.khyzhniak@oneplusone.solutions>
2025-11-22 16:46:10 +0200

    Added profile menu

--- src/app/screens/calendar/calendar.ts
+++ src/app/screens/calendar/calendar.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import {Button} from "@app/components/ui/button/button";
 import {Content} from "@app/components/core/content/content";
 import {Navigation} from "@app/components/ui/navigation/navigation";
 import {img} from '@app/shared/utils/helpers';
@@ -7,7 +6,6 @@ import {img} from '@app/shared/utils/helpers';
 @Component({
   selector: 'app-calendar',
     imports: [
-        Button,
         Content,
         Navigation
     ],
--- src/app/screens/profile/profile.ts
+++ src/app/screens/profile/profile.ts
@@ -1,16 +1,27 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {Content} from "@app/components/core/content/content";
 import {Navigation} from "@app/components/ui/navigation/navigation";
+import {FontAwesomeModule, IconDefinition} from '@fortawesome/angular-fontawesome';
+import {faPencil, faLock, faUserTie, faLanguage, faArrowUpWideShort, faMoneyBill, faBell, faHeadset} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-profile',
-    imports: [
-        Content,
-        Navigation
-    ],
+  standalone: true,
+  imports: [
+    Content,
+    Navigation,
+    FontAwesomeModule
+  ],
   templateUrl: './profile.html',
   styleUrl: './profile.scss',
 })
 export class Profile {
-
+  faPencil: IconDefinition = faPencil;
+  faLock: IconDefinition = faLock;
+  faUserTie: IconDefinition = faUserTie;
+  faLanguage: IconDefinition = faLanguage;
+  faArrowUpWideShort: IconDefinition = faArrowUpWideShort;
+  faMoneyBill: IconDefinition = faMoneyBill;
+  faBell: IconDefinition = faBell;
+  faHeadset: IconDefinition = faHeadset;
 }
