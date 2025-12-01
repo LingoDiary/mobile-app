319972f135e8496a807e4e8b99dce64bf1bfc61a
Yaroslav Khyzhniak <yaroslav.khyzhniak@oneplusone.solutions>
2025-12-01 22:06:48 +0200

    Added virtual scrolling for diary

--- src/app/screens/diary/_parts/entries/entries.ts
+++ src/app/screens/diary/_parts/entries/entries.ts
@@ -1,15 +1,30 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import {DiaryGroup} from '@core/type/diary-group';
-import {formatTime} from '@shared/utils/helpers';
+import { DiaryGroup } from '@core/type/diary-group';
+import { formatTime } from '@shared/utils/helpers';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'app-entries',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [ScrollingModule, RouterLink],
   templateUrl: './entries.html',
   styleUrl: './entries.scss',
 })
 export class Entries {
   @Input({ required: true }) items!: DiaryGroup[];
+
+  @Output() loadMore = new EventEmitter<void>();
+
   protected readonly formatTime = formatTime;
+
+  trackGroup = (index: number, item: DiaryGroup) => item.date;
+
+  onScroll(index: number) {
+    const total = this.items.length;
+
+    if (index > total - 6) {
+      this.loadMore.emit();
+    }
+  }
 }
